fc963e35666cc05a40273ed938b6af99617d9c4f
Alex Stoica <alex_stoicajr@yahoo.co.uk>
2013-06-08 19:22:13 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Stoica Alex's Blog</title>
+	<link rel="stylesheet" href="css/fc-webicons.css">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+
+
+<body>
+	<div class="container">
+
+      <div class="jumbotron">
+			<h1>Hello!</h1>
+			<p class="lead">I make amazing stuff and I am in the process of setting a blog to write about it. For now you can contact me here: </p>
+			<a class="fc-webicon github large" href="https://github.com/alexstoick">Github</a>
+			<a class="fc-webicon google large" href="mailto:alex.stoicajr@gmail.com">Mail</a>
+			<a class="fc-webicon twitter large" href="https://twitter.com/alexstoick">Twitter</a>
+      </div>
+	</div> <!-- /container -->
+
+    <script src='js/modernizr.min.js'></script>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+</body>
+</html>
